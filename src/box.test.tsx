cf0dd723016ccb76290b0b9dcb0a603acfbaa982
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-08-04 20:55:02 +0200

    initial

---
+++ src/box.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box } from './box'
+import { renderToString } from 'react-dom/server'
+
+describe('<Box />', () => {
+  it('no as prop', () => {
+    console.log(renderToString(<div onSubmit={() => alert('blub')} />))
+    // console.log(renderToString(<Box onSubmit={() => alert('blub')} />))
+    console.log(renderToString(<Box as="time" dateTime={'blub'} />))
+    console.log(renderToString(<Box as="a" href="/blub" />))
+  })
+
+  it('input element', () => {
+    console.log(renderToString(<Box as="input" onSubmit={() => alert('blub')} />))
+  })
+
+  it('style prop', () => {
+    console.log(renderToString(<Box p={2} />))
+  })
+})
---
+++ src/box.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { css, cx } from 'emotion'
+
+type Space = 2 | 4 | 8 | 12 | 16 | 24 | 32 | 48 | 64 | 96 | 128 | 256 | 512
+
+type SpaceProps = {
+  p?: Space
+  px?: Space
+  py?: Space
+}
+
+const withSpacingProps = ({ p, px, py }: SpaceProps) => {
+  if (p) return css({ padding: p })
+  if (px) return css({ padding: [0, px, 0, px] })
+  if (py) return css({ padding: [0, py, 0, py] })
+}
+
+type BoxProps<As extends keyof React.ReactHTML> = JSX.IntrinsicElements[As] & {
+  p?: 2 | 4 | 8 | 12
+  as?: As
+}
+
+export function Box<ElementType extends keyof React.ReactHTML | undefined>(
+  props: BoxProps<ElementType extends undefined ? 'div' : ElementType>,
+): JSX.Element {
+  const { as = 'div', className, ...rest } = props
+  const styles = css([withSpacingProps(rest)])
+  return React.createElement(as, { ...rest, className: cx(styles, className) })
+}
